b10b9b2f7961b204898c0ec11bbb1d3b00cc3547
Zak Henry <zak.henry@gmail.com>
2015-06-01 15:51:55 +1000

    Added gulp tasks and repository cloning.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,57 +1,131 @@
 'use strict';
-var yeoman = require('yeoman-generator');
+var generators = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var _ = require('lodash');
+var url = require('url');
+var path = require('path');
 
-module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
+module.exports = generators.Base.extend({
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the stellar ' + chalk.red('spira') + ' generator!'
-    ));
+    constructor: function () {
+        generators.Base.apply(this, arguments);
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+        this.on('error', function(err){
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
+            this.env.error(chalk.magenta("Error: ") + chalk.red(err.message));
 
-      done();
-    }.bind(this));
-  },
+        });
+        // This makes `appname` a required argument.
+        //this.argument('appname', {type: String, required: true});
 
-  writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
+        // And you can then access it later on this way; e.g. CamelCased
+        //this.appnameVar = _.camelCase(this.appname);
+
     },
 
-    projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
-    }
-  },
+    initializing: function(){
 
-  install: function () {
-    this.installDependencies();
-  }
+
+    },
+
+    prompting: function () {
+        var done = this.async();
+
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the stellar ' + chalk.red('spira') + ' generator!'
+        ));
+
+        var prompts = [
+            {
+                type: 'input',
+                name: 'remoteRepo',
+                message: 'Enter the remote repo of your spira project to clone',
+                default: 'https://github.com/spira/spira.git'
+            },
+            {
+                type: 'list',
+                name: 'appFolder',
+                message: 'Choose the directory for your application (select one)',
+                default: './',
+                choices: function(currentAnswers){
+                    var options = [
+                        {
+                            name: ". (Current working directory)",
+                            value: '.'
+                        }
+                    ];
+
+                    var pathInfo = path.parse(currentAnswers.remoteRepo);
+
+                    options.push({
+                        name: pathInfo.name + " (Your repo name)",
+                        value: pathInfo.name
+                    });
+
+                    options.push({
+                        name: "Other",
+                        value: false
+                    });
+
+                    return options;
+                }
+            },
+            {
+                type: 'input',
+                name: 'appFolder',
+                message: 'Enter the folder name you want to create the project in',
+                when: function(currentAnswers){
+
+                    return currentAnswers.appFolder === false;
+                }
+            }
+        ];
+
+        this.prompt(prompts, function (props) {
+            this.props = props;
+            // To access props later use this.props.someOption;
+
+            done();
+        }.bind(this));
+    },
+
+    writing: {
+        //app: function () {
+        //    this.fs.copy(
+        //        this.templatePath('_package.json'),
+        //        this.destinationPath('package.json')
+        //    );
+        //    this.fs.copy(
+        //        this.templatePath('_bower.json'),
+        //        this.destinationPath('bower.json')
+        //    );
+        //},
+        //
+        //projectfiles: function () {
+        //    this.fs.copy(
+        //        this.templatePath('editorconfig'),
+        //        this.destinationPath('.editorconfig')
+        //    );
+        //    this.fs.copy(
+        //        this.templatePath('jshintrc'),
+        //        this.destinationPath('.jshintrc')
+        //    );
+        //}
+
+        projectfiles: function(){
+
+            console.log('this.props', this.props);
+
+        }
+
+    },
+
+    install: function () {
+
+        this.spawnCommand('git', ['clone', this.props.remoteRepo, this.props.appFolder]);
+
+
+    }
 });
