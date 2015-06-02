dc8e809f7ea8037d26a4200dfe3058d1c7a3e07c
Zak Henry <zak.henry@gmail.com>
2015-06-02 18:34:08 +1000

    Installed php dependencies for phpunit tests to run

--- generators/test/index.js
+++ generators/test/index.js
@@ -29,9 +29,16 @@ module.exports = generators.Base.extend({
         },
         'php': {
             command: 'vagrant',
-            args: ['ssh', '--command', 'cd /data && docker-compose run --entrypoint /data/api/vendor/bin/phpunit php --configuration /data/api/phpunit.xml'],
+            args: ['ssh', '--command', 'cd /data && docker-compose run --entrypoint /data/api/vendor/bin/phpunit php --colors --configuration /data/api/phpunit.xml --coverage-clover=/data/reports/coverage/api/clover.xml'],
+            description: "Run PHPUnit tests in docker container"
+        },
+        'phpcoveralls': {
+            command: 'vagrant',
+            args: ['ssh', '--command', 'cd /data && docker-compose run --entrypoint /usr/bin/php lumen /data/vendor/bin/coveralls -v'],
             description: "Run PHPUnit tests in docker container"
         }
+
+        ///data/api/vendor/bin/phpunit --colors --configuration /data/api/phpunit.xml --coverage-clover=/data/reports/coverage/api/clover.xml"
 
     },
 
