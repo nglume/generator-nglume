dfa1fb44537edac8a270b93460271432337df3f6
Zak Henry <zak.henry@gmail.com>
2015-06-24 11:50:29 +1000

    Dropped newman test case

--- generators/test/index.js
+++ generators/test/index.js
@@ -17,17 +17,12 @@ module.exports = generators.Base.extend({
 
     _commands: {
         '_baseCommand' : '',
-        'api': {
-            command: './node_modules/.bin/newman',
-            args: ['-c', './api/postman/spira.json.postman_collection', '-e', './api/postman/spira-local.postman_environment', '--stopOnError'],
-            description: "Run newman integration test"
-        },
         'phplocal': {
             command: './api/vendor/bin/phpunit',
             args: ['--configuration', './api/phpunit.xml'],
             description: "Run PHPUnit tests locally"
         },
-        'php': {
+        'php|api': {
             command: 'vagrant',
             args: ['ssh', '--command', 'cd /data && docker-compose run --entrypoint hhvm php /data/api/vendor/bin/phpunit --colors --configuration /data/api/phpunit.xml --coverage-clover=/data/reports/coverage/api/clover.xml'],
             description: "Run PHPUnit tests in docker container"
