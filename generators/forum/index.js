f860b389e4085b15b7fb61e0605b7be03c3e95eb
Zak Henry <zak.henry@gmail.com>
2015-09-18 14:28:33 +1000

    Fixed forum install command calling itself

--- generators/forum/index.js
+++ generators/forum/index.js
@@ -24,7 +24,7 @@ module.exports = generators.Base.extend({
         },
         'install': {
             command: 'yo',
-            args: ['spira:forum', 'build'],
+            args: ['spira:forum', ' install'], //space is to avoid the recursion
             description: "Install and build forum (alias for yo spira:forum build)"
         },
         '*' : {
